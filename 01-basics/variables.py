f3141071e9fcd6d23668828f229a84ab29468c87
abhishekpandey-devops <abhishekpandey05612@gmail.com>
2026-06-17 09:04:33 +0530

    Added variables example

--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -3,3 +3,34 @@ age = 23
 
 print (name)
 print (age)
+
+#Variable Assignment
+
+Var1 = "Python"
+Var2 = 75
+Var3 = 3.5
+
+#Variable printing
+print(Var1)
+print(Var2)
+print(Var3)
+
+#Multiple Variable Assignment
+a=b=c=65
+
+print (a)
+print (b)
+print (c)
+
+#Multiple Variable Assignment with different values
+x,y,z,a = "alpha", "beta", 12, 5.4
+
+print("Variable x:",x)
+print("Variable y:",y)
+print("Variable z:",z)
+print("Variable a:",a)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
