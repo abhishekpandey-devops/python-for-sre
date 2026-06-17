afc195a0834d82ad8e780df54a0ac9e87ec2f5f2
abhishekpandey-devops <abhishekpandey05612@gmail.com>
2026-06-17 06:38:29 +0530

    Added Python basics examples

---
+++ 01-basics/QuotesAndComments.py
@@ -0,0 +1,19 @@
+#
+# This is a single line comment
+
+"""
+This is a multi line comment
+This is the second line of multi line comment"""
+
+'''This is also a multi line comment
+This is the second line of multi line comment'''
+
+skill="Devops"
+print(skill)  #variable string skill is printed here
+
+print("skill") # string skill is printed here
+print('skill') # string skill is printed here
+
+# string in multiple lines
+print("""This is a string in multiple lines
+      This is the second line of string in multiple lines""")
---
+++ 01-basics/python-syntaxx.py
@@ -0,0 +1,14 @@
+#
+x=10
+print("Learning Indentation in Python")
+print()
+if x==0:
+  print("In the if block")
+  print("Value of x is 0")
+
+else:
+  print("In the else block")
+  print("Value of x is non zero")
+
+print()
+print("This statement is outside the if-else block")
